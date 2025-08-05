d511c767f16be5c4776904edbf42be737ba37573
Mateus Maffi <mateusmaffi@fxsistemas.com.br>
2025-08-05 14:59:33 -0300

    adjust message

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ app.get('/', (req, res) => {
 // Porta do servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+    console.log(`✅ Servidor rodando na porta ${PORT}`);
 });
 
 // Rota de musicas
